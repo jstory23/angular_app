a64dfcf510d7878bf58f4304f1ab43cf1f3f0756
jstory23 <jordanstory23@gmail.com>
2017-03-06 16:01:14 -0700

    updated styles on proposal show, homepage and new, integrated custom font

--- app/proposal/proposal-show.component.js
+++ app/proposal/proposal-show.component.js
@@ -8,7 +8,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var router_1 = require("@angular/router");
 var ProposalShowComponent = (function () {
@@ -27,7 +26,8 @@ ProposalShowComponent = __decorate([
     core_1.Component({
         moduleId: module.id,
         selector: 'proposal-show',
-        templateUrl: 'proposal-show.component.html'
+        templateUrl: 'proposal-show.component.html',
+        styleUrls: ['proposal-show.component.css']
     }),
     __metadata("design:paramtypes", [router_1.ActivatedRoute])
 ], ProposalShowComponent);
